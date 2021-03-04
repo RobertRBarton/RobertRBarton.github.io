c9e50da9bb83905cc17155719d1a80fc51c682b9
Rob Barton <robbybarton3@gmail.com>
2021-03-03 23:06:35 -0600

    fixed all the issues with nav bar and drop down menu. Ready for proper content.

--- static/refs/js/frontendLogic.js
+++ static/refs/js/frontendLogic.js
@@ -1,4 +1,5 @@
 var document;
+var displayDropdown=false;
 
 //These set up the navigation bar
 document.getElementById("home").onclick = function () {
@@ -8,20 +9,16 @@ document.getElementById("home").onclick = function () {
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+	
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+	var content=document.getElementById("myDropdown")
+  if (content.style.display=="inline-block") {
+    content.style.display="none";
+  }else if(event.target.id="Projects"){
+	document.getElementById("myDropdown").style.display="inline-block";
   }
 }
 
